4c56ad0181ffbe29f4b1aee1992591897fd9402a
Bisal-Shafiq <93635077+Bisal-Shafiq@users.noreply.github.com>
2024-06-04 22:08:29 +0500

    Quiz game in python programming

---
+++ quiz-game (1).py
@@ -0,0 +1,350 @@
+score=0
+
+print("welcom to the quiz game")
+print(' ')
+Name=input("Enter your name")
+print('')
+father_name=input('Enter your father name')
+print('')
+play=input('press yes to play a quiz game:')
+print('')
+print('level 1')
+print('')
+print('***********************************************************')
+print('')
+print('here we star a quiz game')
+print('')
+if play=='yes':
+    print('')
+print('***********************************************************')
+print('')
+print('Question 1')
+print('')
+print('***********************************************************')
+print('')
+print("Urdu was declared national language of Pakistan in:")
+print('a) April 1950')
+print('b) April 1955')
+print('c) April 1954')
+print('d) April 1952')
+answer=input('choose to the correct answer:')
+if answer=='c' or answer=='C':
+    score+=1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 2')
+print("In which province maximum languages are spoken?:")
+print('a) Sindh')
+print('b) Punjab')
+print('c) Balochistan')
+print('d) Khyber PK')
+answer=input('choose to the correct answer:')
+if answer=='c' or answer=='C':
+    score=+1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 3')
+print('')
+print("Which Urdu has been taken from the following language:")
+print('a) Persian')
+print('b) Arabic')
+print('c) Turkish')
+print('d) None of these')
+answer=input('choose to the correct answer:')
+if answer=='c' or answer=='C':
+    score=+1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+play=input('you want to play a next level?')
+if play=='yes':
+    print('')
+print('***********************************************************')
+print('')
+print('level 2')
+print('')
+print('***********************************************************')
+print('')
+print('Question 1')
+print('')
+print('***********************************************************')
+print('')
+print("Word Urdu means:")
+print('a) Believers')
+print('b) A group of students')
+print('c) Army')
+print('d) None of these')
+answer=input('choose to the correct answer:')
+if answer=='c' or answer=='C':
+    score+=1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 2')
+print("How many letters are there in Urdu alphabets:")
+print('a) 35')
+print('b) 36')
+print('c) 37')
+print('d) 39')
+answer=input('choose to the correct answer:')
+if answer=='c' or answer=='C':
+    score+=1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 3')
+print('')
+print("Who composed the verses of Pakistan national Anthem?")
+print('a) Hafeez Jallandri')
+print('b) Nasir Kazmi')
+print('c) Allama Iqbal')
+print('d) Faiz Ahmed Faiz')
+answer=input('choose to the correct answer:')
+if answer=='a' or answer=='A':
+    score+=1
+    print('your answer is correct!')
+elif answer=='c' or answer=='C':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+play=input('you want to play a next level?')
+if play=='yes':
+    print('')
+print('***********************************************************')
+print('')
+print('level 3')
+print('')
+print('***********************************************************')
+print('')
+print('Question 1')
+print('')
+print('***********************************************************')
+print('')
+print("Who wrote the national anthem of Pakistan?")
+print('a) Allama Iqbal')
+print('b) Hafeez Jullundari')
+print('c) Mirza Adeeb')
+print('d) Sagar')
+answer=input('choose to the correct answer:')
+if answer=='b' or answer=='B':
+    score+=1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='C' or answer=='c':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 2')
+print("Name the musician who composed the national anthem?")
+print('a) Abdul Karim Chagla')
+print('b) A.Rehman')
+print('c) A.R. Bakshi')
+print('d) Ustad Allah Bux')
+answer=input('choose to the correct answer:')
+if answer=='a' or answer=='A':
+    score+=1
+    print('your answer is correct!')
+elif answer=='c' or answer=='C':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 3')
+print('')
+print("When was the national anthem of Pakistan first played?")
+print('a) 14th August 1948')
+print('b) 13th August 1954')
+print('c) 14th August 1950')
+print('d) 23rd March 1949')
+answer=input('choose to the correct answer:')
+if answer=='b' or answer=='B':
+    score+=1
+    print('your answer is correct!')
+elif answer=='c' or answer=='C':
+    print('your answer is incorrect!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+play=input('you want to play a next level?')
+if play=='yes':
+    print('')
+print('***********************************************************')
+print('')
+print('level 4')
+print('')
+print('***********************************************************')
+print('')
+print('Question 1')
+print('')
+print('***********************************************************')
+print('')
+print("How many lines are in national anthem?")
+print('a) 11')
+print('b) 14')
+print('c) 13')
+print('d) 15')
+answer=input('choose to the correct answer:')
+if answer=='d' or answer=='D':
+    score+=1
+    print('your answer is correct!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='C' or answer=='c':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 2')
+print("Which dam has been built on the Kabul River?")
+print('a) Mangla Dam')
+print('b) Tarbela Dam')
+print('c) Gomal Dam')
+print('d) Warsak Dam')
+answer=input('choose to the correct answer:')
+if answer=='d' or answer=='D':
+    score+=1
+    print('your answer is correct!')
+elif answer=='c' or answer=='C':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 3')
+print('')
+print("What is the name of the combination of smoke and fog?")
+print('a) Smogra')
+print('b) Smogum')
+print('c) Smog')
+print('d) Smoga')
+answer=input('choose to the correct answer:')
+if answer=='c' or answer=='C':
+    score+=1
+    print('your answer is correct!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+print('')
+play=input('you want to play a next level?')
+if play=='yes':
+    print('')
+print('***********************************************************')
+print('')
+print('level 5')
+print('')
+print('***********************************************************')
+print('')
+print('Question 1')
+print('')
+print('***********************************************************')
+print('')
+print("Heer Ranjha” is a love story of:")
+print('a) Punjabi')
+print('b) Sindhi')
+print('c) Pushto')
+print('d) Siraiki')
+answer=input('choose to the correct answer:')
+if answer=='a' or answer=='A':
+    score+=1
+    print('your answer is correct!')
+elif answer=='d' or answer=='D':
+    print('your answer is incorrect!')
+elif answer=='C' or answer=='c':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 2')
+print("Khushta is the first Sahib-e-Dewan poet of:")
+print('a) Pushto')
+print('b) Balochi')
+print('c) Sindhi')
+print('d) Punjabi')
+answer=input('choose to the correct answer:')
+if answer=='d' or answer=='D':
+    score+=1
+    print('your answer is correct!')
+elif answer=='c' or answer=='C':
+    print('your answer is incorrect!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+print('')
+print('***********************************************************')
+print('')
+print('Question 3')
+print('')
+print(". Which peak is located in Karakorum Range")
+print('a) Nanga Patbat')
+print('b) Rakaposhi')
+print('c) Takht-e-Sulaiman')
+print('d) K-2')
+answer=input('choose to the correct answer:')
+if answer=='d' or answer=='D':
+    score+=1
+    print('your answer is correct!')
+elif answer=='b' or answer=='B':
+    print('your answer is incorrect!')
+elif answer=='a' or answer=='A':
+    print('your answer is incorrect!')
+elif answer=='c' or answer=='C':
+    print('your answer is incorrect!')
+print("score",score)
